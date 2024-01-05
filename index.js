1ae49a64975c2d50b2588d78640a7ad09f3a3090
adisorn <adisornsriphongthong@gmail.com>
2024-01-06 00:19:31 +0700

    added

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require('dotenv').config()
 const app = express()
 const port = process.env.port || 3000
 
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
 app.listen(port, '0.0.0.0', (err) => {
     if(err) throw err
     console.log('The server is running on port ' + port)
