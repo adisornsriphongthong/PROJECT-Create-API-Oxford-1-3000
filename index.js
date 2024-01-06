3fb0720b9a92461c03ada092b7a0bde113ba7bed
adisorn <adisornsriphongthong@gmail.com>
2024-01-06 13:44:23 +0700

    added

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-
 const express = require('express');
+const { google } = require('googleapis');
+const { forms } = require('googleapis/build/src/apis/forms');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000; // Use 3000 as a default if PORT is not defined in the environment variables
 
-app.get('/', (req, res) => {
-    res.send('API Oxford 3000')
+async function getAuthSheets() {
+    const auth = new google.auth.GoogleAuth({
+        credentials: JSON.parse(process.env.credentials),
+        scopes: 'https://www.googleapis.com/auth/spreadsheets'
+    })
+
+    const client = await auth.getClient()
+
+    const googleSheets = google.sheets({
+        version: 'v4',
+        auth: client
+    })
+
+    const spreadsheetId = '1tXU8QvGLiJSkS05zjV16pQRqWHREpRk4j3HSe7DuEzs'
+
+    return {
+        auth,
+        client,
+        googleSheets,
+        spreadsheetId
+    }
+}
+
+
+app.get('/', async (req, res) => {
+    const { auth, client, googleSheets, spreadsheetId } = await getAuthSheets()
+    const data = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: 'oxford3000'
+    })
+
+    res.json(data.data)
 });
 
 app.listen(port, '0.0.0.0', (err) => {
